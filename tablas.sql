fdc337a37a3d5ab973fd95a2d38fe7e9aa39ea71
Guillermo-gtz <gmgutierrez@fi.uba.ar>
2026-04-19 13:56:28 -0300

    Añado Tablas.sql al repositorio que automatiza la ejecucion que hace el usuario al crear las tablas por python y no por terminal

---
+++ tablas.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE IF NOT EXISTS prode_api;
+
+CREATE USER IF NOT EXISTS 'prode_user'@'localhost' IDENTIFIED BY 'tu_contraseña_sql';
+GRANT ALL PRIVILEGES ON prode_api.* TO 'prode_user'@'localhost';
+FLUSH PRIVILEGES;
+USE prode_api;
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL 
+    email VARCHAR(255) NOT NULL UNIQUE    
+);
+
+CREATE TABLE IF NOT EXISTS partidos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    equipo_local VARCHAR(255) NOT NULL,
+    equipo_visitante VARCHAR(255) NOT NULL,
+    fecha DATETIME NOT NULL,
+    fase VARCHAR(255) NOT NULL,
+    goles_local INT NULL,
+    goles_visitante INT NULL
+);
+
+CREATE TABLE IF NOT EXISTS predicciones(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    partido_id INT NOT NULL,
+    goles_local INT NOT NULL,
+    goles_visitante INT NOT NULL,
+    UNIQUE KEY
+    uq_usuario_partido (usuario_id, partido_id),
+    CONSTRAIT
+    fk_predicciones_usuario
+    FOREIGN KEY
+    (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+    CONSTRAIT
+    fk_predicciones_partido
+    FOREIGN KEY
+    (partido_id) REFERENCES partidos(id) ON DELETE CASCADE
+);
+
+EXIT;
